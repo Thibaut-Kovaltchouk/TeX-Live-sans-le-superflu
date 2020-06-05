bb8266279e5ad4d0d32bfe9749aebb235c5cb051
Thibaut Kovaltchouk <thibaut.kovaltchouk@gmail.com>
2020-06-06 00:23:03 +0200

    Utilisation de tar plus large que ark

---
+++ 1_getInstallFromLink.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+# on suit le lien du site
+wget http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz 
+# on décompresse l'archive reçue
+tar -xvf install-tl-unx.tar.gz
+# on supprime l'archive
+rm install-tl-unx.tar.gz
+
--- getInstallFromLink.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-# on suit le lien du site
-wget http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
-# on décompresse l'archive reçue
-ark --batch install-tl-unx.tar.gz
-rm install-tl-unx.tar.gz
